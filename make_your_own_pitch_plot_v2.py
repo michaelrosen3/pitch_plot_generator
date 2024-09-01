61d9403b3c3a583209e83f7ff42683f91fa2c8fd
michaelrosen3 <michaelrosen3@gmail.com>
2024-09-01 14:03:26 -0700

    Update make_your_own_pitch_plot_v2.py

--- make_your_own_pitch_plot_v2.py
+++ make_your_own_pitch_plot_v2.py
@@ -28,7 +28,7 @@ def my_hash_func(obj):
 @st.cache(hash_funcs={types.FunctionType: my_hash_func})
 def load_data():
     # Use a relative path to the pickle file
-    file_path = os.path.join('data', 'statcast_data.pkl')
+    file_path = os.path.join('data', 'pitch_plot_data_v2.pkl')
     return pd.read_pickle(file_path)
 
 # Load data once
