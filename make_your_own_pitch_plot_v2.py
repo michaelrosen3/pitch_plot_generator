1d1edfaa52444d86e21480fece4dca252d822a5a
michaelrosen3 <michaelrosen3@gmail.com>
2024-09-03 10:12:15 -0700

    Update make_your_own_pitch_plot_v2.py

--- make_your_own_pitch_plot_v2.py
+++ make_your_own_pitch_plot_v2.py
@@ -18,7 +18,7 @@ def my_hash_func(obj):
 
 @st.cache(hash_funcs={types.FunctionType: my_hash_func})
 def load_data():
-    url = 'https://github.com/michaelrosen3/pitch_plot_generator/blob/main/pitch_plot_data_excel.xlsx?raw=true'
+    url = 'https://github.com/michaelrosen3/pitch_plot_generator/blob/main/pitch_plot_data_excel_v2.xlsx?raw=true'
     response = requests.get(url)
     response.raise_for_status()  # Check for HTTP errors
     return pd.read_excel(BytesIO(response.content))
@@ -92,9 +92,34 @@ player_names = sorted(statcast_data['player_name'].unique())
 # Dropdown menu for player name selection
 pitcher_name = st.selectbox('Search player name', [''] + player_names)
 
+# Define firm start and end dates for the slider
+firm_start_date = pd.to_datetime('2024-03-28')
+firm_end_date = pd.to_datetime('2024-08-31')
+
+# Date range slider with default range from March 28, 2024, to August 31, 2024
+date_range = st.slider(
+    'Select date range',
+    min_value=firm_start_date,
+    max_value=firm_end_date,
+    value=(firm_start_date, firm_end_date),
+    format="YYYY-MM-DD"
+)
+
+start_date, end_date = date_range
+
 # Generate plot when the button is clicked
 if st.button('Generate Pitch Plot'):
-    if pitcher_name in statcast_data['player_name'].unique():
-        plot_pitch_movement(pitcher_name)
+    if pitcher_name and pitcher_name in statcast_data['player_name'].unique():
+        # Filter data based on the selected player and date range
+        filtered_data = statcast_data[
+            (statcast_data['player_name'] == pitcher_name) & 
+            (statcast_data['game_date'] >= start_date) & 
+            (statcast_data['game_date'] <= end_date)
+        ]
+        
+        if not filtered_data.empty:
+            plot_pitch_movement(filtered_data, pitcher_name)
+        else:
+            st.write('No data available for the selected date range.')
     else:
         st.write('Player not found. Please check the name and try again.')
